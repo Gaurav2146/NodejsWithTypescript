95b84dd316f0b270a373d1b46e6410de1084e27e
Gaurav Kumar <GauravKumar@winjittechnologiespvtltd.local>
2023-12-12 14:09:10 +0530

    changes done

--- src/Service/order-service.ts
+++ src/Service/order-service.ts
@@ -1,16 +1,40 @@
 import { User } from "../entity/User";
 import {DatabaseFactory} from "../factory/databaseFactory";
 
+
 export class OrderService{
 
     async createOrder(name:string,price:number)
     {
-        const user = new User();
-        user.name = name;
-        user.price = price;
+        try{
 
-        return await DatabaseFactory.getDataSource().manager.save(user);
-        //return DatabaseFactory.AppDataSource.manager.create(User,{name:name,price:price});
+            const user1 = new User();
+        user1.name = name;
+        user1.price = price;
+
+        const user2 = new User();
+        user2.name = name;
+        user2.price = price;
+
+        //IMPLEMENTING TRANSACTION and ISOLATION LEVELS
+        return await DatabaseFactory.getDataSource().manager.transaction("SERIALIZABLE" ,async (transactionalEntityManager) => {
+
+            await transactionalEntityManager.save(user1);
+
+            await transactionalEntityManager.save(user2);
+
+        })
+
+        // INSERTING DATA WITHOUT USING TRANSACTION
+        // return await DatabaseFactory.getDataSource().manager.save(user1);
+        // return DatabaseFactory.AppDataSource.manager.create(User,{name:name,price:price});
+
+        }
+        catch(error)
+        {
+            throw new Error();
+        }
+        
     }
 
 }
