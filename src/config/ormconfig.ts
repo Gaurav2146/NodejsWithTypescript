b2e92af5175170188762eeab6f648b6612d3ee81
Gaurav Kumar <GauravKumar@winjittechnologiespvtltd.local>
2023-12-13 14:03:23 +0530

    changes done

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -8,8 +8,10 @@ export const configuration:DataSourceOptions = {
     password: "Gau@9931",
     database: "typeORM",
     logging: true,
+    logger: "file",
     synchronize: false,
     entities: [
         "./src/entity/User.ts"
     ]
 }
+
